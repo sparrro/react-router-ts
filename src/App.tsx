40d9ef0cd9690c3171e6aaa12637a6c6421f972f
Kaayzeer <mautiagiro@gmail.com>
2024-02-14 11:07:22 +0100

    router functionality and root layout component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
-import Nav from "./components/Nav/Nav";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import RootLayout from "./components/Layout/RootLayout";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+      </Route>
+    </Route>
+  )
+);
 
 function App() {
   return (
-    <header>
-      <Nav />
-    </header>
+    <>
+      <RouterProvider router={router} />
+    </>
   );
 }
 
--- src/components/constants/constants.ts
+++ src/components/constants/constants.ts
@@ -1,5 +1,5 @@
 export const navigationLinks = [
   { name: "Home", href: "/" },
   { name: "Contact", href: "/contact" },
-  { name: "About", href: "/about" },
+  { name: "Products", href: "/products" },
 ];
